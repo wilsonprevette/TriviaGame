9d465403e53986663244737242450c5900ba2e50
Wilson P <wilson.prevette@gmail.com>
2018-06-05 01:47:47 -0500

    minor changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,4 @@
-//creating function for generating results when submit button is clicked
+//create function for generating results when submit button is clicked
 
 function getResults(){
     //creating variables for questions/correct responses and number correct/incorrect
@@ -14,38 +14,40 @@ function getResults(){
 
     //set if statements for correct answers
 
-    if (question1 === "Halloween 3") {
+    if (question1 == "Halloween 3") {
         correct++;
     }
 
-    if (question1 === "Italy") {
+    if (question1 == "Italy") {
         correct++;
     }
 
-    if (question1 === "Stuart Gordon") {
+    if (question1 == "Stuart Gordon") {
         correct++;
     }
 
-    if (question1 === "Scream") {
+    if (question1 == "Scream") {
         correct++;
     }
 
-    if (question1 === "Roger Corman") {
+    if (question1 == "Roger Corman") {
         correct++;
     }
 
-    if (question1 === "Mall") {
+    if (question1 == "Mall") {
         correct++;
     }
 
-    if (question1 === "The Howling") {
+    if (question1 == "The Howling") {
         correct++;
     }
 
-    if (question1 === "John Carpenter") {
+    if (question1 == "John Carpenter") {
         correct++;
     }
+
+    //making results div visible after submit button is clicked, change message accordingly
 
     document.getElementById("results").style.visibility = "visible";
-    document.getElementById("numberCorrect").innherHTML = "You got " + correct + " correct.";
+    document.getElementById("numberCorrect").innerHTML = "You got" + correct + " correct!";
 }
